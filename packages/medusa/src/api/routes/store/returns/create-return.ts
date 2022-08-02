82089c5428cbf144c80cfbf481f2976a52c1a89d
adrien2p <adrien.deperetti@gmail.com>
2022-08-02 09:32:06 +0200

    fix(medusa): post rebase

--- packages/medusa/src/api/routes/store/returns/create-return.ts
+++ packages/medusa/src/api/routes/store/returns/create-return.ts
@@ -187,7 +187,7 @@ export default async (req, res) => {
 
                   return {
                     response_code: 200,
-                    response_body: { return: ret },
+                    response_body: { return: returnOrder },
                   }
                 }
               )
